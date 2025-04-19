57158765755d496b98e482b9afd710cf11e2c7db
Nomena Fitahiana <nomenafitahiana.randrianirina@gmail.com>
2025-04-19 11:13:10 +0300

    feat: create all classes for the processing time

---
+++ src/main/java/com/example/demo/entity/BestProcessingTime.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class BestProcessingTime {
+   private String salesPoint;
+   private String dish;
+   private double preparationDuration;
+   private DurationUnit durationUnit;
+}
